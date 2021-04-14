9703b290966e42199ce1e8827abee2059015ef54
Aleksey Sobchenko <aleksobchenko@gmail.com>
2021-04-15 00:16:27 +0300

    01-dz
    12

--- js/01-dz.js
+++ js/01-dz.js
@@ -1,8 +1,127 @@
-const productName = 'Droid';
-console.log(productName);
+// 1
 
-// const pricePerItem = 2000;
+// const productName = 'Droid';
 
 // console.log(productName);
 
+// const pricePerItem = 2000;
+
 // console.log(pricePerItem);
+
+// 2;
+
+// // Start code
+// let productName = 'Droid';
+// let pricePerItem = 2000;
+
+// // Write your code under this line
+
+// productName = 'Repair droid';
+// pricePerItem = 2000 + 1500;
+
+// console.log(productName);
+// console.log(pricePerItem);
+
+// 3;
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = 'mango935';
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
+
+// 4;
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// console.log(totalPrice);
+
+// 5;
+
+// const productName = 'Дроид';
+// const pricePerItem = 3500;
+
+// const message = `Вы выбрали ${productName}, цена за штуку ${pricePerItem} кредитов`;
+
+// console.log(message);
+
+// 6;
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+
+// console.log(pricePerDroid);
+// console.log(orderedQuantity);
+// console.log(deliveryFee);
+// console.log(totalPrice);
+// console.log(message);
+
+// 7;
+
+// function sayHi() {
+//   console.log('Привет, это моя первая функция!');
+// }
+
+// sayHi();
+
+// 8;
+
+// function add(a, b, c) {
+//   console.log(`Результат сложения равен ${a + b + c}`);
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// 9;
+
+// function add(a, b, c) {
+//   return a + b + c;
+//   console.log('${ a + b + c}');
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// 10
+
+// function makeMessage (name, price) {
+//    const message = `Вы выбрали ${name}, цена за штуку ${price} кредитов` ;
+//   return message;
+// };
+
+// 11;
+
+// function calculateTotalPrice(orderedQuantity, pricePerItem) {
+//   const totalPrice = orderedQuantity * pricePerItem;
+
+//   return totalPrice;
+// }
+
+// 12;
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   const message = `Вы заказали дроидов на сумму ${
+//     orderedQuantity * pricePerDroid + deliveryFee
+//   } кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+
+//   return message;
+// }
+
+13;
