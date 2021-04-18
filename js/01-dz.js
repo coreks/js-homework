af233f31e22a8fcafb2a538625edc588fab51b06
Aleksey Sobchenko <aleksobchenko@gmail.com>
2021-04-18 23:43:18 +0300

    36 ok

--- js/01-dz.js
+++ js/01-dz.js
@@ -285,3 +285,183 @@
 // }
 
 25;
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Пиши код ниже этой строки
+//   message =
+//     ordered > available
+//       ? 'На складе недостаточно товаров!'
+//       : 'Заказ оформлен, с вами свяжется менеджер';
+//   // Пиши код выше этой строки
+//   return message;
+// }
+
+26;
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+//   // Пиши код ниже этой строки
+//   message =
+//     password === ADMIN_PASSWORD
+//       ? 'Доступ разрешен'
+//       : 'Доступ запрещен, неверный пароль!';
+//   // Пиши код выше этой строки
+//   return message;
+// }
+
+27;
+
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Пиши код ниже этой строки
+
+//   switch (
+//     type // Дополни эту строку
+//   ) {
+//     case 'starter': // Дополни эту строку
+//       price = 0; // Дополни эту строку
+//       break;
+
+//     case 'professional': // Дополни эту строку
+//       price = 20; // Дополни эту строку
+//       break;
+
+//     case 'organization': // Дополни эту строку
+//       price = 50; // Дополни эту строку
+//       break;
+//   }
+
+//   // Пиши код выше этой строки
+//   return price;
+// }
+
+28;
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+
+//   switch (password) {
+//     case null:
+//       message = 'Отменено пользователем!';
+//       break;
+
+//     case ADMIN_PASSWORD:
+//       message = 'Добро пожаловать!';
+//       break;
+
+//     default:
+//       message = 'Доступ запрещен, неверный пароль!';
+//   }
+
+//   return message;
+// }
+
+29;
+
+// function getShippingCost(country) {
+//   let message;
+//   // Пиши код ниже этой строки
+//   switch (country) {
+//     case 'Китай':
+//       message = 'Доставка в Китай будет стоить 100 кредитов';
+//       break;
+
+//     case 'Чили':
+//       message = 'Доставка в Чили будет стоить 250 кредитов';
+//       break;
+
+//     case 'Австралия':
+//       message = 'Доставка в Австралия будет стоить 170 кредитов';
+//       break;
+
+//     case 'Ямайка':
+//       message = 'Доставка в Ямайка будет стоить 120 кредитов';
+//       break;
+
+//     default:
+//       message = 'Извините, в вашу страну доставки нет';
+//   }
+
+//   // Пиши код выше этой строки
+//   return message;
+// }
+
+30;
+
+// function getNameLength(name) {
+//   const message = `Длина вашего имени ${name.length} символа(ов)`; // Дополни эту строку
+
+//   return message;
+// }
+
+31;
+
+// const courseTopic = 'JavaSript для начинающих';
+// // Пиши код ниже этой строки
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[courseTopic.length-1];
+
+// // Пиши код выше этой строки
+
+32;
+
+// function getSubstring(string, length) {
+//   const substring = string.slice(0, length); // Дополни эту строку
+
+//   return substring;
+// }
+
+33;
+
+// const message = 'Привет привет привет привет';
+// const maxLength = 10;
+
+// function formatMessage(message, maxLength) {
+//   let result;
+//   // Пиши код ниже этой строки
+
+//   if (message.length <= maxLength) {
+//     result = message;
+//   } else {
+//     result = message.slice(0, maxLength) + '...';
+//   }
+
+//   // Пиши код выше этой строки
+//   return result;
+// }
+
+34;
+
+// function normalizeInput(input) {
+//   const normalizedInput = input.toLowerCase(); // Дополни эту строку
+//   return normalizedInput;
+// }
+
+35;
+
+// function checkForName(fullName, name) {
+//   const result = fullName.includes(name); // Дополни эту строку
+//   return result;
+// }
+
+36;
+
+// function checkForSpam(message) {
+//   let result;
+//   // Пиши код ниже этой строки
+//   if (message.toLowerCase().includes('spam')) {
+//     result = true;
+//   } else if (message.toLowerCase().includes('sale')) {
+//     result = true;
+//   } else {
+//     result = false;
+//   }
+
+//   // Пиши код выше этой строки
+//   return result;
+// }
