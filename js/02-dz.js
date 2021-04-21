d329ae3d1cd4a14d5a9f3190a8bac5f99f9c6939
Aleksey Sobchenko <aleksobchenko@gmail.com>
2021-04-22 00:20:15 +0300

    Module 2
    exc 13 ok

--- js/02-dz.js
+++ js/02-dz.js
@@ -8,3 +8,140 @@
 
 //   return 'Вы не совершеннолетний человек';
 // }
+
+2;
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   // Пиши код ниже этой строки
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Добро пожаловать!';
+//   }
+//   return 'Доступ запрещен, неверный пароль!';
+
+//   // Пиши код выше этой строки
+// }
+
+3;
+
+// function checkStorage(available, ordered) {
+//   // Пиши код ниже этой строки
+
+//   if (ordered === 0) {
+//     return 'В заказе еще нет товаров';
+//   }
+//   if (ordered > available) {
+//     return 'Слишком большой заказ, на складе недостаточно товаров!';
+//   }
+//   return 'Заказ оформлен, с вами свяжется менеджер';
+
+//   // Пиши код выше этой строки
+// }
+
+4;
+
+// Пиши код ниже этой строки
+
+// Пиши код ниже этой строки
+
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин']
+
+5;
+
+// // Стартовый код
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+// // Пиши код ниже этой строки
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[fruits.length - 1];
+
+// console.table(fruits);
+
+// console.log(firstElement);
+// console.log(secondElement);
+// console.log(lastElement);
+
+6;
+
+// Стартовый код
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+// // Пиши код ниже этой строки
+
+// fruits[1] = 'персик';
+// fruits[3] = 'банан';
+
+// console.log(fruits);
+
+7;
+
+// // Стартовый код
+// const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+// // Пиши код ниже этой строки
+// const fruitsArrayLength = fruits.length
+
+8;
+
+// // Стартовый код
+// const fruits = ['яблоко', 'персик', 'груша', 'банан'];
+// // Пиши код ниже этой строки
+
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// console.log(lastElementIndex);
+// console.log(lastElement);
+
+9;
+
+// function getExtremeElements(array) {
+//   // Пиши код ниже этой строки
+
+//   const deletedArray = array.splice(1, array.length - 2);
+
+//   return array;
+// }
+
+10;
+
+// function splitMessage(message, delimeter) {
+//   let words;
+//   // Пиши код ниже этой строки
+//   words = message.split(delimeter);
+
+//   // Пиши код выше этой строки
+//   return words;
+// }
+
+11;
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Пиши код ниже этой строки
+//   let total = message.split(' ').length * pricePerWord;
+
+//   return total;
+//   // Пиши код выше этой строки
+// }
+
+12;
+
+// function makeStringFromArray(array, delimeter) {
+//   let string;
+//   // Пиши код ниже этой строки
+
+//   string = array.join(delimeter);
+
+//   // Пиши код выше этой строки
+//   return string;
+// }
+
+13;
+
+// function slugify(title) {
+//   //   // Пиши код ниже этой строки
+
+//   let slug = title.toLowerCase().split(' ').join('-');
+
+//   return slug;
+//   //   // Пиши код выше этой строки
+// }
